495ac6ac86a9f7b27fd5da14ee0db2f87c0b659b
Tuan <nbtuan59k3cntt.dhv@gmail.com>
2022-07-13 17:30:27 +0700

    relationship 1

--- app/Models/Info.php
+++ app/Models/Info.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Info extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'id_number',
+        'phone',
+        'address',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->hasOne('App\Models\User::class', 'user_id', 'id');
+    }
 }
